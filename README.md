7d27609b9ef368082f763aee8c1b748142901d9f
Sebastião Rodrigol <rodrigoexer1@gmail.com>
2025-01-05 21:19:10 -0300

    projeto crud starting

---
+++ README.md
@@ -0,0 +1,7 @@
+### Projeto Next.JS Banco Postgress
+
+
+#### Projeto aluno Carlos
+* 15º mês
+
+<img src="./screens/page1.png" alt="não carregou">
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,8 @@
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="bg-black h-screen w-screen">
+      <h1 className="text-center text-4xl mt-10">Home</h1>
     </div>
   );
 }
